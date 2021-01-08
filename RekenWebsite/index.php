e9cde0e6602593983dc226755463000119e0a9ae
Nugget <cdl.040404@gmail.com>
2021-01-08 08:05:23 +0100

    quick

--- RekenWebsite/index.php
+++ RekenWebsite/index.php
@@ -9,46 +9,26 @@ if (isset($_SESSION["leerling"])) { $_SESSION["leerling"] = "";}
 
 <head>
     <link rel="stylesheet" href="CSS/style.css">
-    <link rel="stylesheet" href="CSS/IndexStyle.css">
+    <link rel="stylesheet" href="CSS/header.css">
+    <link rel="stylesheet" href="CSS/main.css">
     <title>Home</title>
 </head>
 
 <body>
-    <div class="top_header">
-        <div class="left">
-            <div class="left_content">
-                <img style="width: 50px; height: 50px;" src="IMG/HOME.png">
-            </div>
-        </div>
-        <div class="mid">
-            <div class="mid_content">
-                <p class="mid-text" style="margin: 22px;">REKENWEBSITE</p>
-            </div>
-        </div>
-        <div class="right">
-            <div class="right_content">
-                <img style="width: 60px; height: 60px;" src="IMG/LOGO.png">
-            </div>
-        </div>
-    </div>
+    <header class="page-header">
+        <a href="/RekenSite/RekenWebsite/"><img src="IMG/HOME.png"></a>
+        <a href="/RekenSite/RekenWebsite/"><img src="IMG/back.png"></a>
+        <a href="/RekenSite/RekenWebsite/"><h1>RekenWebsite</h1></a>
+        <a href="/RekenSite/RekenWebsite/"><img src="IMG/LOGO.png"></a>
+    </header>
 
-    <form action="Connect/login.php" method="POST">
-        <div class="login">
-            <p class="h1-login">LOG IN</p>
-            <div class="name-div">
-                <label class="Lnaam">
-                    <P>Naam</P>
-                </label>
-                <input class="name" type="text" placeholder="Naam" name="Naam" required>
-            </div>
-            <div class="ww-div">
-                <label class="Lwachtwoord">
-                    <P>Wachtwoord</P>
-                </label>
-                <input class="password" type="password" placeholder="Wachtwoord" name="Pass" required>
-            </div>
-            <input type="submit">
-        </div>
+    <form class="login" action="Connect/login.php" method="POST">
+        <h1>LOG IN</h1>
+        <label>Naam</label>
+        <input class="name" type="text" placeholder="Naam" name="Naam" required>
+        <label>Wachtwoord</label>
+        <input class="password" type="password" placeholder="Wachtwoord" name="Pass" required>
+        <input type="submit" value="verzenden">
     </form>
 </body>
 
--- RekenWebsite/opgaven.php
+++ RekenWebsite/opgaven.php
@@ -6,13 +6,13 @@ session_start();
 
 <head>
     <title>Document</title>
-    <link rel="stylesheet" href="CSS/IndexStyle.css">
+    <link rel="stylesheet" href="CSS/header.css">
     <link rel="stylesheet" href="CSS/style.css">
     <link rel="stylesheet" href="CSS/opgavenStyle.css">
 </head>
 
 <body>
-    <div class="top_header">
+    <!-- <div class="top_header">
         <div class="left">
             <div class="left_content">
                 <a href="index.php"><img style="width: 50px; height: 50px;" src="IMG/HOME.png"></a>
@@ -38,48 +38,65 @@ session_start();
                     <img style="width: 60px; height: 60px;" src="IMG/LOGO.png">
             </div>
         </div>
-    </div>
+    </div> -->
+    <header class="page-header">
+        <a href="/RekenSite/RekenWebsite/"><img src="IMG/HOME.png"></a>
+        <a href="/RekenSite/RekenWebsite/"><img src="IMG/back.png"></a>
+        <a href="/RekenSite/RekenWebsite/"><h1>
+            <?php
+                if (isset($_SESSION["leerling"])) {
+                    $array = $_SESSION["leerling"]; 
+                    echo $array[0];
+                }
+                else {
+                    echo "geen naam gevonden";
+                }
+            ?>
+        </h1></a>
+        <a href="/RekenSite/RekenWebsite/"><img src="IMG/LOGO.png"></a>
+    </header>
+
     <div class="pageWrapper">
         <div class="item plus">
-            <div class="title">
-                <h1>PLUS</h1>
-                <h1>+</h1>
-            </div>
-            <hr>
-            <p>tot en met:</p>
             <form action="opgaven/plus.php">
+                <div class="title">
+                    <h1>PLUS</h1>
+                    <h1>+</h1>
+                </div>
+                <hr>
+                <p>tot en met:</p>
                 <p>
                     <input type="radio" id="10plus" class="select" name="max" value="10"><label for="10plus">10, </label>
                     <input type="radio" id="100plus" class="select"name="max" value="100"><label for="100plus">100, </label>
                     <input type="radio" id="1000plus" class="select"name="max" value="1000"><label for="1000plus">1000</label>
                 </p>
-                <input class="submit" type="submit">
+                <input class="submit" type="submit" value="verzenden">
             </form>
         </div>
         <div class="item min">
-            <div class="title">
-                <h1>MIN</h1>
-                <h1>-</h1>
-            </div>
-            <hr>
-            <p>tot en met:</p>
             <form action="opgaven/min.php">
+                <div class="title">
+                    <h1>MIN</h1>
+                    <h1>-</h1>
+                </div>
+                <hr>
+                <p>tot en met:</p>
                 <p>
                     <input type="radio" id="10min" class="select" name="max" value="10"><label for="10min">10, </label>
                     <input type="radio" id="100min" class="select"name="max" value="100"><label for="100min">100, </label>
                     <input type="radio" id="1000min" class="select"name="max" value="1000"><label for="1000min">1000</label>
                 </p>
-                <input class="submit" type="submit">
+                <input class="submit" type="submit" value="verzenden">
             </form>
         </div>
         <div class="item keer">
-            <div class="title">
-                <h1>KEER</h1>
-                <h1>x</h1>
-            </div>
-            <hr>
-            <p>de tafel van:</p>
             <form action="opgaven/keer.php">
+                <div class="title">
+                    <h1>KEER</h1>
+                    <h1>x</h1>
+                </div>
+                <hr>
+                <p>de tafel van:</p>
                 <p>
                     <input type="radio" id="1keer" class="select" name="tafel" value="1"><label for="1keer">1, </label>
                     <input type="radio" id="2keer" class="select" name="tafel" value="2"><label for="2keer">2, </label>
@@ -92,33 +109,31 @@ session_start();
                     <input type="radio" id="9keer" class="select" name="tafel" value="9"><label for="9keer">9, </label>
                     <input type="radio" id="10keer" class="select" name="tafel" value="10"><label for=10keer">10</label>
                 </p>
-                <input class="submit" type="submit">
+                <input class="submit" type="submit" value="verzenden">
             </form>
         </div>
         <div class="item delen">
-            <div class="title">
-                <h1>DELEN</h1>
-                <h1>:</h1>
-            </div>
-            <hr>
-            <p>tot en met:</p>
-            <div class="form-wrapper">
-                <form action="opgaven/delen.php">
-                    <p>
-                        <input type="radio" id="1delen" class="select" name="tafel" value="1"><label for="1delen">1, </label>
-                        <input type="radio" id="2delen" class="select" name="tafel" value="2"><label for="2delen">2, </label>
-                        <input type="radio" id="3delen" class="select" name="tafel" value="3"><label for="3delen">3, </label>
-                        <input type="radio" id="4delen" class="select" name="tafel" value="4"><label for="4delen">4, </label>
-                        <input type="radio" id="5delen" class="select" name="tafel" value="5"><label for="5delen">5, </label>
-                        <input type="radio" id="6delen" class="select" name="tafel" value="6"><label for="6delen">6, </label>
-                        <input type="radio" id="7delen" class="select" name="tafel" value="7"><label for="7delen">7, </label>
-                        <input type="radio" id="8delen" class="select" name="tafel" value="8"><label for="8delen">8, </label>
-                        <input type="radio" id="9delen" class="select" name="tafel" value="9"><label for="9delen">9, </label>
-                        <input type="radio" id="14delen" class="select" name="tafel" value="10"><label for="10delen">10</label>
-                    </p>
-                    <input class="submit" type="submit">
-                </form>
-            </div>
+            <form action="opgaven/delen.php">
+                <div class="title">
+                    <h1>DELEN</h1>
+                    <h1>:</h1>
+                </div>
+                <hr>
+                <p>tot en met:</p>
+                <p>
+                    <input type="radio" id="1delen" class="select" name="tafel" value="1"><label for="1delen">1, </label>
+                    <input type="radio" id="2delen" class="select" name="tafel" value="2"><label for="2delen">2, </label>
+                    <input type="radio" id="3delen" class="select" name="tafel" value="3"><label for="3delen">3, </label>
+                    <input type="radio" id="4delen" class="select" name="tafel" value="4"><label for="4delen">4, </label>
+                    <input type="radio" id="5delen" class="select" name="tafel" value="5"><label for="5delen">5, </label>
+                    <input type="radio" id="6delen" class="select" name="tafel" value="6"><label for="6delen">6, </label>
+                    <input type="radio" id="7delen" class="select" name="tafel" value="7"><label for="7delen">7, </label>
+                    <input type="radio" id="8delen" class="select" name="tafel" value="8"><label for="8delen">8, </label>
+                    <input type="radio" id="9delen" class="select" name="tafel" value="9"><label for="9delen">9, </label>
+                    <input type="radio" id="14delen" class="select" name="tafel" value="10"><label for="10delen">10</label>
+                </p>
+                <input class="submit" type="submit" value="verzenden">
+            </form>
         </div>
     </div>
 </body>
